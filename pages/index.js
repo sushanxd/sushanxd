8d539abb491c904b72fcd884740cde8032b13a21
Sushan <132475630+sushanxd@users.noreply.github.com>
2025-10-23 19:22:08 +0100

    fix import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import ProfileHeader from '@/components/ProfileHeader'
+import ProfileHeader from '../components/ProfileHeader'
 
 export default function Home() {
   return (
